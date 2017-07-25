1681d26abcdad28ea7d6aa76fc7629b1d78b6f02
Tarmin Sidharta <tsidharta@gmail.com>
2017-07-24 18:01:26 -0700

    submit

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,28 @@
+
+var friendData = require("../data/friend.js");
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+
+// A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
+	app.get("/api/friends", function(req, res) {
+    	res.json(friendData);
+    });
+
+// A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used 
+// to handle the compatibility logic.
+	app.post('/api/friends', function(req, res){
+
+
+
+
+	});
+
+
+
+
+};
